f7a93d6b79958a2fbd2b4b0e8188d37c53a02166
小轲博客 <2501521908@qq.com>
2019-05-20 15:14:59 +0800

    更新了配置

---
+++ src/test/java/cn/keshaowl/page/login/TestUser.java
@@ -0,0 +1,17 @@
+package cn.keshaowl.page.login;
+
+import org.junit.Test;
+/**
+ * 项目的测试类
+ * 用户
+ * @author shuaike
+ *
+ */
+public class TestUser {
+	
+	@Test
+	public void init() {
+		System.out.println("github:https://github.com/a2501521908/qqauth");
+	}
+
+}
